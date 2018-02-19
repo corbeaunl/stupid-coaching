73ce59102e13b29bb0656113106208d48cfb7d3f
Alexander Lancee <alexlancee@gmail.com>
2018-02-19 17:30:52 +0100

    stupid-coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'pages/ask', to: 'pages#ask', as: :ask
+
+  get 'pages/answer', to: 'pages#answer', as: :answer
+
+  get 'questions', to: 'pages#questions',
+
+  get 'greetings', to: 'pages#greetings',
+
+  root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
